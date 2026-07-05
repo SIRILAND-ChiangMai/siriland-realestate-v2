f1a5a321fa12b42827867436a83bd2debb9080f7
SIRILAND-ChiangMai <ayhanelif111@gmail.com>
2026-07-05 11:41:44 +0700

    Update properties.js

--- properties.js
+++ properties.js
@@ -1,16 +1,30 @@
-const properties = [
-  {
-    title: "Wiang Ping Mansion Room 1110",
-    city: "Chiang Mai",
-    type: "Condo",
-    deal: "Sale",
-    price: "2.59 MB",
-    description: "Near MAYA only 200 m, 11th floor, renovated, foreign quota, Airbnb allowed.",
-    bedrooms: "1 Bedroom",
-    bathrooms: "1 Bathroom",
-    area: "36 sq.m.",
-    images: ["images/wiang-ping-1110-1.jpg"]
-  },
+{
+  title: "Wiang Ping Mansion Room 1110",
+  city: "Chiang Mai",
+  type: "Condo",
+  deal: "Sale",
+  price: "2.59 MB",
+
+  description:
+    "Luxury renovated condominium only 200 meters from MAYA Lifestyle Shopping Center. 1 Bedroom, 1 Bathroom, 36 sq.m., 11th Floor. Foreign Quota available. Airbnb allowed. Free transfer.",
+
+  bedrooms: "1 Bedroom",
+  bathrooms: "1 Bathroom",
+  area: "36 sq.m.",
+
+  images: [
+    "images/wiang-ping-1110-1.jpg",
+    "images/wiang-ping-1110-2.jpg",
+    "images/wiang-ping-1110-3.jpg",
+    "images/wiang-ping-1110-4.jpg",
+    "images/wiang-ping-1110-5.jpg",
+    "images/wiang-ping-1110-6.jpg",
+    "images/wiang-ping-1110-7.jpg",
+    "images/wiang-ping-1110-8.jpg",
+    "images/wiang-ping-1110-9.jpg",
+    "images/wiang-ping-1110-10.jpg"
+  ]
+},
   {
     title: "JC Hill Place Room 909",
     city: "Chiang Mai",
